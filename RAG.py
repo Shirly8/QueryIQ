9efd8d7bb33bc37fc0792899f5ca385ac72c1a65
“Shirley” <ShirleyHuang7@hotmail.com>
2024-08-19 16:55:12 -0400

    Added Chroma Vector Database

--- RAG.py
+++ RAG.py
@@ -59,3 +59,76 @@ def createIds(chunks):
     return chunks
 
 
+#Intialize Ollama vector embeddings from text data
+def get_embedding():
+    embeddings = OllamaEmbeddings(
+        model = "nomic-embed-text"
+    )
+    return embeddings
+
+
+#Initialize database:
+def initialize_chroma():
+    global db
+
+    if db is None:
+        db = Chroma(
+            persist_directory = CHROMA_PATH, 
+            embedding_function= get_embedding()
+        )
+    return db
+
+
+#Manage vector database with ChromaDB storing embeddings (Source path, page number, chunk number)
+def add_to_chroma(chunks: list[Document]):
+
+    db = initialize_chroma()
+
+    #Assign page IDs
+    id_chunks = createIds(chunks)
+
+    #Adding documents
+    existing_items = db.get(include = [])
+    existing_ids = set(existing_items["ids"]) if existing_items else set()
+    print(f"Number of documents in the database: {len(existing_ids)}")
+
+    #Creating chunks (That aren't in the DB)
+    new_chunks = []
+    for chunk in id_chunks:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if new_chunks:
+        print(f"New Documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+    else:
+        print("No new documents to add")
+
+
+def clear():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+def main():
+
+    #Parse command-line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+
+    # Check if the database should be cleared (using the --reset flag)
+    if args.reset:
+        print("✨ Clearing Database")
+        clear()
+
+    # Load documents, split into chunks, and add them to Chroma
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+
+if __name__ == "__main__":
+    main()
+
+
